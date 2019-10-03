5a54ea4572073faa8d1259742952661ef86fbfdf
Lassi <lassi_mustonen@hotmail.com>
2019-10-03 11:33:25 +0300

    backend aloitettu ja eka data haettu ja parsittu

--- back-end/index.js
+++ back-end/index.js
@@ -1,12 +1,56 @@
 const express = require('express');
 const app = express();
+const Request = require('request');
+
+const salaryData = [
+  {
+    avgSalary: 42000,
+    country: 'fin'
+  },
+  {
+    avgSalary: 40000,
+    country: 'swe'
+  },
+  {
+    avgSalary: 80000,
+    country: 'nor'
+  },
+  {
+    avgSalary: 39000,
+    country: 'den'
+  }
+];
+
+// api end point for fetching salary
+// So far it know only finlands stats
+app.get('/salary', (req, res) => {
+  Request.get(
+    'https://api.worldbank.org/v2/countries/fin/indicators/NY.ADJ.NNTY.PC.CD?format=json',
+    (error, response, body) => {
+      if (error) {
+        return console.log(error);
+      }
+      // here data is parsed forom extra information
+      let data = JSON.parse(body);
+      data = data[1];
+      let resp = data.map(year => {
+        return {
+          country: year.country,
+          year: year.date,
+          value: year.value
+        };
+      });
+      res.json(resp);
+    }
+  );
+});
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>');
 });
 
-app.get('/notes', (req, res) => {
-  res.json(notes);
+app.get('/salarytest', (req, res) => {
+  res.json(salaryData);
 });
 
 const PORT = 3001;
