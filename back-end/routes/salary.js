31bbdfa99d687e26343c30182589066e4eafc2bf
Joni Kiviluoto <joni.kiviluoto@gmail.com>
2019-10-09 11:54:46 +0300

    Back-end
    salaryinfon haku lisätty salary.js:ään
    löytyy osoitteesta http://localhost:3001/salary/salaryinfo

--- back-end/routes/salary.js
+++ back-end/routes/salary.js
@@ -2,17 +2,35 @@ const express = require('express');
 const app = express();
 const Request = require('request');
 const router = express.Router();
+const address = 'https://api.worldbank.org/v2/countries/fin/indicators/NY.ADJ.NNTY.PC.CD?format=json';
+
+router.get('/salaryinfo', (req, res) => {
+    Request.get(
+      address,
+      (error, response, body) => {
+        if (error) {
+          return console.log(error);
+        }
+        // here data is parsed from extra information
+        let data = JSON.parse(body);
+        // prints all the data into console
+        // console.log(data)
+        data = data[0];        
+        res.json(data);
+      }
+    );
+  });
 
 // api end point for fetching salary
 // So far it know only finlands stats
 router.get('/', (req, res) => {
     Request.get(
-      'https://api.worldbank.org/v2/countries/fin/indicators/NY.ADJ.NNTY.PC.CD?format=json',
+      address,
       (error, response, body) => {
         if (error) {
           return console.log(error);
         }
-        // here data is parsed forom extra information
+        // here data is parsed from extra information
         let data = JSON.parse(body);
         // prints all the data into console
         // console.log(data)
