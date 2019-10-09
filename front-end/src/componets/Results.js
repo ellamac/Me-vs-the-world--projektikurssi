4f027bc43df452a0f5d628142ce27c94cf5a587b
Ella Mäkelä <ella@makela.co>
2019-10-09 09:38:39 +0300

    Resultsissa piirakkakaavion mockup. Committaajan spostin muutos

--- front-end/src/componets/Results.js
+++ front-end/src/componets/Results.js
@@ -1,16 +1,50 @@
-import React from 'react';
-import ProgressBar from 'react-bootstrap/ProgressBar';
+import React from "react";
+import { VictoryPie, VictoryLabel, VictoryTooltip } from "victory";
 
-const Results = () => {
-  const now = 60;
-  const soon = 80;
-  return (
-    <>
-      <h2 className='text-center mb-5'>Tulokset</h2>
-      <ProgressBar className='mb-5' now={now} label={`${now}%`} />
-      <ProgressBar now={soon} label={`${soon}%`} />
-    </>
-  );
-};
+class CustomLabel extends React.Component {
+  render() {
+    return (
+      <g>
+        <VictoryLabel {...this.props} />
+        <VictoryTooltip
+          {...this.props}
+          x={200}
+          y={250}
+          orientation="top"
+          pointerLength={0}
+          cornerRadius={50}
+          flyoutWidth={100}
+          text="jtn"
+          flyoutHeight={100}
+          flyoutStyle={{ fill: "orange" }}
+        />
+      </g>
+    );
+  }
+}
+
+CustomLabel.defaultEvents = VictoryTooltip.defaultEvents;
+
+class Results extends React.Component {
+  render() {
+    const data = [
+      { country: "fin", avgSalary: 42000 },
+      { country: "swe", avgSalary: 40000 },
+      { country: "nor", avgSalary: 80000 },
+      { country: "den", avgSalary: 39000 }
+    ];
+    return (
+      <VictoryPie
+        style={{ labels: { fill: "white" } }}
+        innerRadius={100}
+        labelRadius={120}
+        labelComponent={<CustomLabel />}
+        data={data}
+        x="country"
+        y="avgSalary"
+      />
+    );
+  }
+}
 
 export default Results;
