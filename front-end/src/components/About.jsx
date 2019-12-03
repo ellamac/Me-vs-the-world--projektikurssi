daa2ad32b99ffe92199bc5bff4703fbc2c9f2f12
Ella Mäkelä <ella@makela.co>
2019-12-03 09:49:19 +0200

    logoscg added to public

--- front-end/src/components/About.jsx
+++ front-end/src/components/About.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import logo from '../images/logosvg.svg';
+import fullLogo from '../images/logo_white.png';
 
 const About = () => {
   const imgStyle = {
-    width: '50%',
+    width: '100%',
     maxWidth: '1005px',
     height: 'auto',
     padding: '0px 0px 50px 0px'
@@ -22,14 +23,14 @@ const About = () => {
       <div style={centered}>
         <img
           style={imgStyle}
-          src={logo}
+          src={fullLogo}
           alt='Me-VS-World'
           padding='100px 0px 0px 0px'
         />
-        <h1>ME VS THE WORLD </h1>
-        <p style={{ color: 'white' }}>
-          University of Jyväskylä, Open Data Student Project Fall 2019 (TIEA207)
-        </p>
+        <h5 style={{ color: 'white' }}>
+          University of Jyväskylä, Open Data Student Project, Fall 2019,
+          (TIEA207)
+        </h5>
       </div>
     </Container>
   );
